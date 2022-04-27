a27f4bcb344c224c4ab07faf8d30bdaf14cf577b
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-27 23:24:38 +0430

    application is set

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	app := echo.New()
+
+	app.GET("/time", func(c echo.Context) error {
+		return c.String(http.StatusOK, time.Now().GoString())
+	})
 
 	app.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello world")
