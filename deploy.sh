756c1e61d801c09825651ec499c4f851a5441c51
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-20 11:55:14 +0330

    update: deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/official-stallion/template.git
+
+GIT_TEMPL="https://github.com/official-stallion/template.git"
+
+
+# clone into directory
+git clone "$GIT_TEMPL"
+
 
 cd template
 
+# install dependencies
 npm i
 
+# build project
 npm run build
 
 cd ..
 
+# move files
 mv template/dist/* .
 
 mkdir assets
 
 mv template/dist/assets/* ./assets
 
+# remove all
 rm -rf template
